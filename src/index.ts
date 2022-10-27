42f257970758707ab0432145971f43ee4f68406f
liuqiutao <qiutao.liu@7x-networks.com>
2022-10-27 18:26:42 +0800

    feat:引入原子css

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,14 @@ import { createApp } from "vue";
 import SButton from "./button";
 import SFCButton from "./SFCButton.vue";
 import JSXButton from "./JSXButton";
-createApp(JSXButton).mount("#app");
+import SmartyUI from "./entry";
+
+createApp({
+  template: `
+        <div>
+            <SButton>普通按钮</SButton>
+        </div>
+    `,
+})
+  .use(SmartyUI)
+  .mount("#app");
