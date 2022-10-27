27884be9b66ded6aeac6bd1a7babbc99518096f2
liuqiutao <qiutao.liu@7x-networks.com>
2022-10-27 17:23:30 +0800

    feat: 项目初始化

--- vite.config.ts
+++ vite.config.ts
@@ -1,56 +1,34 @@
-/// <reference types="vitest" />
+import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import Unocss from "./config/unocss";
-import { defineConfig, Plugin } from "vite";
+
 const rollupOptions = {
   external: ["vue", "vue-router"],
   output: {
-    assetFileNames: `assets/[name].css`,
     globals: {
       vue: "Vue",
     },
   },
 };
-export const config = {
+
+export default defineConfig({
   plugins: [
-    vue() as Plugin,
+    vue(),
     // 添加JSX插件
-    vueJsx() as Plugin,
-    // 添加UnoCSS插件
-    Unocss() as Plugin[],
+    vueJsx({
+      // options are passed on to @vue/babel-plugin-jsx
+    }),
   ],
   // 添加库模式配置
   build: {
     rollupOptions,
-    minify: "terser", // boolean | 'terser' | 'esbuild'
-    sourcemap: true, // 输出单独 source文件
-    reportCompressedSize: true, // 生成压缩大小报告
+    minify: false,
     lib: {
       entry: "./src/entry.ts",
-      name: "UIDemo",
-      fileName: "ui-demo-lqt",
+      name: "SmartyUI",
+      fileName: "smarty-ui",
       // 导出模块格式
-      formats: ["esm", "umd", "iife"], // 导出模块类型
-    },
-    cssCodeSplit: true,
-    outDir: "./dist",
-  },
-  resolve: {
-    alias: {
-      vue: "vue/dist/vue.esm-bundler.js",
+      formats: ["es", "umd", "iife"],
     },
   },
-  test: {
-    // enable jest-like global test APIs
-    globals: true,
-    // simulate DOM with happy-dom
-    // (requires installing happy-dom as a peer dependency)
-    environment: "happy-dom",
-    // 支持tsx组件，很关键
-    transformMode: {
-      web: [/.[tj]sx$/],
-    },
-  },
-};
-export default defineConfig(config);
+});
