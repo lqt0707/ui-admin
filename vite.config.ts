216c382c2d2837d803b2f34902e7484a88d2e8ab
lqt0707 <1444706897@qq.com>
2022-10-29 15:30:28 +0800

    feat: npm publish conf

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
-import { presetUno, presetAttributify, presetIcons } from "unocss";
 import Unocss from "./config/unocss";
 
 const rollupOptions = {
@@ -27,13 +26,15 @@ export default defineConfig({
   // 添加库模式配置
   build: {
     rollupOptions,
-    minify: false,
+    minify: "terser", // boolean | 'terser' | 'esbuild'
+    sourcemap: true, // 输出单独 source文件
+    reportCompressedSize: true, // 生成压缩大小报告
     lib: {
       entry: "./src/entry.ts",
       name: "SmartyUI",
       fileName: "smarty-ui",
       // 导出模块格式
-      formats: ["es", "umd", "iife"],
+      formats: ["es", "umd", "iife"], // 导出模块类型
     },
     cssCodeSplit: true,
   },
